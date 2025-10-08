8b89e8388d2ee969a93f6fd97a1b4be5f405e09c
Dev Kumar Singh <gohi772004@gmail.com>
2025-10-09 03:22:09 +0530

    feat: Enhance review routes with additional endpoints for user reviews and average rating

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
-import { addReview, getReviewsByBookId, updateReview } from "../controllers/reviewController.js";
-const router = Router();
+import { 
+  addReview, 
+  deleteReview, 
+  getBookAverageRating, 
+  getReviewsByBookId, 
+  getReviewsByUser,
+  getReviewById,
+  updateReview 
+} from "../controllers/reviewController.js";
 
+const router = Router();
 
 router.post("/add", addReview);
 router.get("/book/:bookId", getReviewsByBookId);
+router.get("/user/:userId", getReviewsByUser);       
+router.get("/review/:reviewId", getReviewById);      
+router.get("/average/:bookId", getBookAverageRating);
 router.put("/:id", updateReview);
-
-
+router.delete("/:id", deleteReview);
 
 export default router;
